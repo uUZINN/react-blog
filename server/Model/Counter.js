f59b84d01c3b5bf70e439f9b2c913186765fe1a6
L-jy16 <89904583+uUZINN@users.noreply.github.com>
2023-12-11 16:16:52 +0900

    2023.12.11

---
+++ server/Model/Counter.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const countSchema = new mongoose.Schema(
+    {
+        name: String,
+        postNum: Number,
+    },
+    { collection: "counter" });
+
+const Counter = mongoose.model("Counter", countSchema);
+
+module.exports = { Counter };
